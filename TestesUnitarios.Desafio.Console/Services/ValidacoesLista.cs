d8e2396fd52702b8796951add7b99c20ada23d4e
Cristian0-Pereira <cristianops2020@gmail.com>
2024-11-14 21:10:52 -0300

    Up
    Simplificando o código.

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -3,31 +3,18 @@ namespace TestesUnitarios.Desafio.Console.Services
     public class ValidacoesLista
     {
         public List<int> RemoverNumerosNegativos(List<int> lista)
-        {
-            var listaSemNegativos = lista.Where(x => x > 0);
-            return listaSemNegativos.ToList();
-        }
+        => lista.Where(x => x > 0).ToList();
 
         public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
-        {
-            var contem = lista.Contains(numero);
-            return contem;
-        }
+        => lista.Contains(numero);
 
         public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
-        {
-            var listaMultiplicada = lista.Select(x => x * numero).ToList();
-            return listaMultiplicada;
-        }
+        => lista.Select(x => x * numero).ToList();
 
         public int RetornarMaiorNumeroLista(List<int> lista)
-        {
-            return lista.Max();
-        }
+        => lista.Max();
 
         public int RetornarMenorNumeroLista(List<int> lista)
-        {
-            return lista.Min();
-        }
+        => lista.Min();
     }
 }
--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -3,21 +3,12 @@ namespace TestesUnitarios.Desafio.Console.Services
     public class ValidacoesString
     {
         public int RetornarQuantidadeCaracteres(string texto)
-        {
-            var numeroCaracteres = texto.Length;
-            return numeroCaracteres;
-        }
+        => texto.Length;
 
         public bool ContemCaractere(string texto, string textoProcurado)
-        {
-            var contem = texto.Contains(textoProcurado);
-            return contem;
-        }
+        => texto.Contains(textoProcurado);
 
         public bool TextoTerminaCom(string texto, string textoProcurado)
-        {
-            var termina = texto.EndsWith(textoProcurado);
-            return termina;
-        }
+        => texto.EndsWith(textoProcurado);
     }
 }
